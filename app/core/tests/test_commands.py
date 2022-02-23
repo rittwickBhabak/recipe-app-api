46c10e3919314aeca4c0d2cde05b875d6756a48d
Rittwick Bhabak <rittwick2019@gmail.com>
2022-02-23 18:39:49 +0530

    configured postgres db

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,30 @@
+from subprocess import call
+from unittest.mock import patch 
+
+from django.core.management import call_command 
+from django.db.utils import OperationalError 
+from django.test import TestCase
+
+class CommandTests(TestCase):
+
+    # this function specifies what will happen when we call
+    # 'wait_for_db' when the database is already available
+    def test_wait_for_db_ready(self):
+        """Test waiting for when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True 
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+
+    # 'wait_for_db' command will try to connect the db for
+    # five times and at sixth time it will be successful
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
+
+
